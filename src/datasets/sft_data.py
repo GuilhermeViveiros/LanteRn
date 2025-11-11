6b0c4288e35128faf630d23bf90d32a67c658fdf
GuilhermeViveiros <aviveiros2012@gmail.com>
2025-11-11 13:47:11 +0000

    training scripts

--- src/datasets/sft_data.py
+++ src/datasets/sft_data.py
@@ -1,67 +1,123 @@
 """
 Dataset for supervised fine-tuning (SFT)
 """
+from functools import partial
 import json
 import os
 from PIL import Image
-from datasets import Dataset
-from typing import Dict
+from torch.utils.data import Dataset
+from typing import Any, Dict, List, Optional, Union
 import torch
 from src.utils import center_and_crop_image
+from transformers import AutoProcessor
+from qwen_vl_utils import process_vision_info
 
 
-class SFTData(Dataset):
-    def __init__(self, data_path: str, visual_model: torch.nn.Module):
-        #super().__init__()
-        # load json data
+class SFTDataset(Dataset):
+    def __init__(
+        self,
+        data_path: str,
+        vision_model: torch.nn.Module,
+        processor: AutoProcessor
+    ):
+        super(SFTDataset, self).__init__()
+        self.processor = processor
         with open(data_path, "r") as f:
             self.dataset = json.load(f)
-        # visual model should be a model that takes an image and returns the patch features (the visual part of Qwen2.5VL for instance)
-        self.visual_model = visual_model
-
+    
     def __len__(self):
         return len(self.dataset)
 
-    def get_visual_features(self, img: Image.Image) -> torch.Tensor:
-        # get the visual features
-        import pdb; pdb.set_trace()
-        visual_features = self.visual_model(img)
-        return visual_features
-
-    def __getitem__(self, idx) -> Dict[str, torch.Tensor]:
+    def __getitem__(self, idx):
         # retrieve the image
         data = self.dataset[idx]
-        # extract the image patch features
-        img = Image.open(data["img_path"])
-        img_bboxs = [center_and_crop_image(img, bbox) for bbox in data["bboxs"]]
         
-        # lantern is divided into three components
-        # 1. the vq sample (image + question)
-        # 2. the optional pre-reasoning trace before the latent visual reasoning
-        import pdb; pdb.set_trace()
-        # get the image patch features
-        img_path = os.path.join(self.img_folder_path, data["dataset"], data["image"])
-        if not os.path.exists(img_path):
-            raise FileNotFoundError(f"Image path {img_path} does not exist")
-        img = Image.open(img_path)
-        # save img
-        #img.save("img.jpg")
-        img_bboxes = [center_and_crop_image(img, bbox) for bbox in data["bboxs"]]    
+        # Extract data fields
+        question = data["question"]
+        reasoning_traces = data["reasoning_traces"]
+        answer = reasoning_traces["answer"]
+        pre_visual_latent_reasoning = reasoning_traces.get("pre_visual_text_think", None)
+        post_visual_latent_reasoning = reasoning_traces.get("post_visual_text_think", None)
+        text_only_reasoning = reasoning_traces.get("text_think", None)
+        
+        # Validate reasoning traces
+        if text_only_reasoning is None:
+            assert post_visual_latent_reasoning is not None or pre_visual_latent_reasoning is not None, \
+                "If text_reasoning is not None, post_visual_latent_reasoning or pre_visual_latent_reasoning must be not None"
+            
+        # Extract the image and process bboxes
+        img = Image.open(data["img_path"])
+        # Build user content
+        user_content = [
+            {"type": "text", "text": question},
+            {"type": "image", "image": img},
+        ]
 
-        return Sample(
-            image=img, 
-            img_bboxes=img_bboxes, 
-            img_path=img_path,
-            question=data["question"],
-            answer=data["answer"],
-            dataset=data["dataset"],
-            split=data["split"],
-            bboxs=data["bboxs"]
-        )
+        # Build assistant content
+        assistant_content = []
+        if text_only_reasoning is not None:
+            # just text without latent visual reasoning
+            assistant_content.append({"type": "text", "text": text_only_reasoning})
+        else:
 
+            img_bboxs = [center_and_crop_image(img, bbox) for bbox in data["bboxs"]]
+            # Validate bbox count matches post_reasoning_trace count
+            assert len(img_bboxs) == len(post_visual_latent_reasoning), \
+                f"The number of bboxs ({len(img_bboxs)}) and post_visual_latent_reasoning ({len(post_visual_latent_reasoning)}) must be the same"
+
+
+            # Build assistant content by adding pre_visual_latent_reasoning, and interleaving bbox images with post_visual_latent_reasoning
+            if pre_visual_latent_reasoning is not None:
+                assistant_content.append({"type": "text", "text": pre_visual_latent_reasoning})
+            for img_bbox, post_visual_latent_reasoning in zip(img_bboxs, post_visual_latent_reasoning, strict=True):
+                assistant_content.append({"type": "image", "image": img_bbox})
+                assistant_content.append({"type": "text", "text": post_visual_latent_reasoning})
+            
+        # Add the final answer
+        assistant_content.append({"type": "text", "text": answer})
+
+        # Create the message
+        message = [{
+            "role": "user",
+            "content": user_content,
+        }, {
+            "role": "assistant",
+            "content": assistant_content,
+        }]
+
+        return message
+
+def collate_fn(samples: List[str], processor: AutoProcessor):
+    # apply the tokenizer
+    text = processor.apply_chat_template(samples, tokenize=False)
+    # process the vision info
+    image_inputs, video_inputs = process_vision_info(samples)
+    inputs = processor(
+        text=text,
+        images=image_inputs,
+        videos=video_inputs,
+        padding=True,
+        return_tensors="pt",
+    )
+    import pdb; pdb.set_trace()
+    return inputs
+    
+def make_sft_data_module(vision_model, processor, data_path):
+    """Make dataset and collator for supervised fine-tuning."""
+    sft_dataset = SFTDataset(
+        data_path=data_path, processor=processor, vision_model=vision_model
+    )
+
+    return {
+        "train_dataset": sft_dataset,
+        "eval_dataset": None,
+        "data_collator": partial(collate_fn, processor=processor)
+    }   
 
 if __name__ == "__main__":
-    data_path = "/mnt/data-artemis/gviveiros/lantern/LantErn_VisCot_data.json"
+    
+    data_path="/mnt/data-artemis/gviveiros/lantern/LantErn_VisCot_data.json"
+    
 
     # load the visual model
     from transformers import Qwen2_5_VLForConditionalGeneration, AutoProcessor
@@ -70,8 +126,17 @@ if __name__ == "__main__":
     #    model_id, torch_dtype="auto", device_map="auto"
     #)
     processor = AutoProcessor.from_pretrained(model_id)
-    data = SFTData(data_path, None)
-    for i in range(10):
-        data[i]
+    data_module = make_sft_data_module(
+        vision_model=None, 
+        processor=processor, 
+        data_path=data_path
+    )
+
+    # test with the dataloader
+    from torch.utils.data import DataLoader
+    dataloader = DataLoader(data_module["train_dataset"], batch_size=8, collate_fn=data_module["data_collator"])
+    for batch in dataloader:
+        print(batch.keys())
         break
+    
     import pdb; pdb.set_trace()
--- src/train/train.py
+++ src/train/train.py
@@ -1,6 +1,12 @@
 import logging
 from transformers import HfArgumentParser
 from src.params import (TrainingParams, ModelParams, DataParams)
+from src.datasets.sft_data import make_sft_data_module
+
+from transformers import Qwen2_5_VLForConditionalGeneration, AutoProcessor
+from transformers import SFTConfig
+from src.trainer.trainer import LantErnSFTrainer
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("LantErn-Train")
@@ -11,19 +17,28 @@ def train(training_params: TrainingParams, model_params: ModelParams, data_param
     logger.info(f"Model parameters: {model_params}")
     logger.info(f"Data parameters: {data_params}")
 
-    # Load model
-    model = AutoModelForCausalLM.from_pretrained(model_params.model_name)
 
-
-if __name__ == "__main__":
-    parser = HfArgumentParser((TrainingParams, ModelParams, DataParams))
-    training_params, model_params, data_params = parser.parse_args_into_dataclasses()
-    # LantErn just supports Qwen models
-    if "Qwen" not in model_params.model_name:
-        raise ValueError(f"LantErn only supports Qwen models, got {model_params.model_name}")
+    # Load Model
+    model = Qwen2_5_VLForConditionalGeneration.from_pretrained(model_params.model_id)
+    processor = AutoProcessor.from_pretrained(model_params.model_id)
     
     # Load data
-    data = load_dataset("json", data_files=data_params.data_path)
-    
-    train(training_params, model_params, data_params)
-    import pdb; pdb.set_trace()
+    train_dataset, \
+        eval_dataset, \
+            data_collator = make_sft_data_module(
+                vision_model=model.vision_model,
+                processor=processor,
+                data_path=data_params.data_path
+            )
+
+    # Train
+    trainer = LantErnSFTrainer(
+        model,
+        args=SFTConfig(max_length=None),
+        train_dataset=train_dataset,
+        eval_dataset=None,
+        data_collator=data_collator,
+        training_args=training_params
+    )
+
+    trainer.train()
