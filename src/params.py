bd5e78c0c3a0a55828bc49ab2b06fa68b83c1209
GuilhermeViveiros <aviveiros2012@gmail.com>
2025-12-12 10:19:41 +0000

    preparing to merge

--- src/params.py
+++ src/params.py
@@ -6,8 +6,9 @@ from transformers import TrainingArguments as HFTrainingArguments
 class ModelParams:
     model_id: str = field(default="Qwen/Qwen2.5-VL-3B-Instruct")
     latent_size: int = field(default=4)
-    use_cache: bool = field(default=False)
-
+    use_cache: bool = field(default=True)
+    attn_implementation: str = field(default="flash_attention_2")
+    
 @dataclass
 class TrainingParams(HFTrainingArguments):
     #run_name: str = field(default="LantErn-SFT-Qwen2.5VL-3B")
@@ -21,7 +22,7 @@ class TrainingParams(HFTrainingArguments):
     gamma: float = field(default=0.1) # weight for the latpent similarity loss
     gradient_checkpointing: bool = field(default=True)
     fp16: bool = field(default=False)
-    max_steps: int = field(default=400) # -1 for no max steps
+    max_steps: int = field(default=-1) # -1 for no max steps
     bf16: bool = field(default=True)
     report_to: str = field(default="wandb")
     wandb_project: str = field(default="LantErn-SFT")
@@ -31,11 +32,14 @@ class TrainingParams(HFTrainingArguments):
     freeze_merger: bool = field(default=True)
     freeze_llm: bool = field(default=False)
     eval_strategy: str = field(default="steps")
-    eval_steps: int = field(default=50)
-    test_steps: int = field(default=1)
+    eval_steps: int = field(default=100)
+    test_steps: int = field(default=100)
     dataloader_num_workers: int = field(default=4)
     dataloader_persistent_workers: bool = field(default=True)
     save_safetensors: bool = field(default=True)
+    seed: int = field(default=42)
+    #use_liger_kernel: bool = field(default=True) # LantErn does not support liger kernel
+    #use_liger_kernel: bool = field(default=True)
     # per_device_train_batch_size: int = field(default=8)
     # gradient_accumulation_steps: int = field(default=1)
 
@@ -46,6 +50,6 @@ class TrainingParams(HFTrainingArguments):
 class DataParams:
     data_path: str = field(default="/mnt/data-artemis/gviveiros/lantern/LantErn_VisCot_data.json")
     dummy: bool = field(default=False)
-    #split_percentages: Tuple[float, float, float] = field(default=(0.9, 0.0998, 0.0002))
     shuffle_dataset: bool = field(default=True)
-    split_percentages: Tuple[float, float, float] = field(default=(0.9, 0.0905, 0.005))
+    #split_percentages: Tuple[float, float, float] = field(default=(0.9, 0.1, 0.0))
+    split_percentages: Tuple[float, float, float] = field(default=(0.9, 0.097, 0.003))
